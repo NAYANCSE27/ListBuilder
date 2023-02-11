9a2ac07c940cc528b9f07b066f9b5c08ceab0220
NAYANCSE27 <mehadihasannayan1998@gmail.com>
2023-02-12 00:14:35 +0600

    formatted the code

--- src/output/Output.jsx
+++ src/output/Output.jsx
@@ -4,15 +4,12 @@ const Output = ({ listMessage, setListMessage }) => {
   return (
     <div className="table__value">
       <ul>
-        {
-          listMessage.map((message, index) =>(
-            <li key={index}>{message}</li>
-          ))
-        }
+        {listMessage.map((message, index) => (
+          <li key={index}>{message}</li>
+        ))}
       </ul>
     </div>
   );
-}
+};
 
 export default Output;
-
