693584cd4773e01d9808dd89f5b0d75fcde556e1
NAYANCSE27 <mehadihasannayan1998@gmail.com>
2023-02-14 00:10:58 +0600

    added to-do list

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
-import { useState } from "react";
-import Input from "./input/Input";
-import Output from "./output/Output";
+import Todo from "./components/Todo";
 
 import "./App.css";
 
 function App() {
-  // const [newMessage, setNewMessage] = useState('');
-  const [listMessage, setListMessage] = useState([]);
   return (
     <div className="App">
-      <Input listMessage={listMessage} setListMessage={setListMessage} />
-      <Output listMessage={listMessage} setListMessage={setListMessage} />
+      <Todo />
     </div>
   );
 }
